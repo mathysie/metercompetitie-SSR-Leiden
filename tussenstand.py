40d909bf982fccefd79c649cb6689fea1aea2320
Mathijs Kolkhuis Tanke <mathijs@kolkhuistanke.nl>
2018-11-14 19:06:02 +0100

    Lees tussenstand of vanuit bestand of vanuit website

--- tussenstand.py
+++ tussenstand.py
@@ -1,8 +1,33 @@
 import requests
 import re
+import sys
 
 bestuur = 'Blijf Genieten'
 website = 'https://metercompetitie.ssr-leiden.nl/'
+
+
+def read_input_file():
+    data = []
+
+    f = open('input.txt', 'r')
+    for line in f:
+        data.append(line.rstrip().split(','))
+
+    return data
+
+
+def read_input_site():
+    pattern = '<span class="dispuutsnaam">([a-zA-Z0-9.\- ]+)</span>[\r\n\s]+' \
+        + '<span class="aantalMeters">([0-9]+)</span>'
+
+    site = requests.get(website).text
+    matches = re.findall(pattern, site, re.MULTILINE)
+
+    return matches
+
+
+def read_input():
+    return read_input_site() if from_site else read_input_file()
 
 
 def lootjes(meters):
@@ -10,18 +35,14 @@ def lootjes(meters):
 
 
 def parse_disputen():
-    pattern = '<span class="dispuutsnaam">([a-zA-Z0-9.\- ]+)</span>[\r\n\s]+' \
-        + '<span class="aantalMeters">([0-9]+)</span>'
     disputen = {}
+    data = read_input()
 
-    site = requests.get(website).text
-    matches = re.findall(pattern, site, re.MULTILINE)
-
-    for match in matches:
-        if match[0] != bestuur:
-            disputen[match[0]] = {
-                'meters': int(match[1]),
-                'lootjes': lootjes(int(match[1]))
+    for entry in data:
+        if entry[0] != bestuur:
+            disputen[entry[0]] = {
+                'meters': int(entry[1]),
+                'lootjes': lootjes(int(entry[1]))
             }
 
     disputen = sorted(
@@ -31,3 +52,6 @@ def parse_disputen():
     )
 
     return disputen
+
+
+from_site = len(sys.argv) >= 2 and sys.argv[1] == '1'
